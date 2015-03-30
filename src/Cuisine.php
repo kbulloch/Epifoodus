48c402a6ec83a6609f6b8a19dd681e277e475b2a
Kyle <student@epicodus.com>
2015-03-30 15:22:03 -0700

    Add Cuisine.php class file

---
+++ src/Cuisine.php
@@ -0,0 +1,19 @@
+<?php
+
+    class Cuisine
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+    }
+?>
