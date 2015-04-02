8908f81a0ef6f9c9c02c4cbb3f13f31896b72ac7
Evan Butler <evanb2@mac.com>
2015-04-01 20:32:42 -0700

    clean whitespace

--- src/User.php
+++ src/User.php
@@ -1,213 +1,213 @@
 <?php
-class User
-{
-	private $username;
-	private $password;
-	private $id;
-	private $vegie;
-	private $admin;
+	class User
+	{
+		private $username;
+		private $password;
+		private $id;
+		private $vegie;
+		private $admin;
 
-	function __construct($new_username, $new_password, $new_vegie, $new_admin, $new_id = null)
-	{
-		$this->id       = $new_id;
-		$this->password = $new_password;
-		$this->username = $new_username;
-		$this->vegie    = $new_vegie;
-		$this->admin    = $new_admin;
-	}
-	//setters
-	function setUsername($new_username)
-	{
-		$this->username = $new_username;
-	}
-
-	function setPassword($new_password)
-	{
-		$this->password = $new_password;
-	}
-
-	function setVegie($new_vegie)
-	{
-		$this->vegie = $new_vegie;
-	}
-
-	function setAdmin($new_admin)
-	{
-		$this->admin = $new_admin;
-	}
-
-	function setId($new_id)
-	{
-		$this->id = (int) $new_id;
-	}
-	//getters
-	function getUsername()
-	{
-		return $this->username;
-	}
-
-	function getPassword()
-	{
-		return $this->password;
-	}
-
-	function getVegie()
-	{
-		return $this->vegie;
-	}
-
-	function getAdmin()
-	{
-		return $this->admin;
-	}
-
-	function getId()
-	{
-		return $this->id;
-	}
-
-	function save()
-	{
-		$statemnt = $GLOBALS['DB']->query("INSERT INTO users (username, password,vegie,admin) VALUES
-			('{$this->getUsername()}', '{$this->getPassword()}',{$this->getVegie()},{$this->getAdmin()}) RETURNING id;");
-		$result   = $statemnt->fetch(PDO::FETCH_ASSOC);
-		$this->setId($result['id']);
-	}
-
-	function updateUsername($user_name)
-	{
-		$GLOBALS['DB']->exec("UPDATE users SET username={$user_name} WHERE id={$this->getId()};");
-		$this->setUsername($user_name);
-	}
-
-	function updatePassword($new_password)
-	{
-		$GLOBALS['DB']->exec("UPDATE users SET password={$new_password} WHERE id={$this->getId()};");
-		$this->setPassword($new_password);
-	}
-
-	function updateAnswer($answer, $rest_id)
-	{
-		$GLOBALS['DB']->exec("UPDATE likes SET answer = {$answer} Where user_id={$this->getId() } AND restaurant_id={$rest_id};");
-	}
-
-	function addAnswer($user_id, $res_id, $answer)
-	{
-		$GLOBALS['DB']->exec(" INSERT INTO likes (answer, user_id, restaurant_id)
-			VALUES ($answer,{$this->getId()},$res_id)");
-	}
-
-	function delete()
-	{
-		$GLOBALS['DB']->exec("DELETE FROM users * WHERE id={$this->getId()};");
-	}
-
-	function getLikes()
-	{
-		$user_likes = $GLOBALS['DB']->query("SELECT restaurants.* FROM users
-			JOIN likes ON (users.id = likes.user_id)
-			JOIN restaurants ON (likes.restaurant_id = restaurants.id)
-			WHERE likes.answer = 2 AND likes.user_id = {$this->getId()};");
-		$likes = array();
-		foreach ($user_likes as $restaurant) {
-			$name           = $restaurant['name'];
-			$address        = $restaurant['address'];
-			$phone          = $restaurant['phone'];
-			$price          = $restaurant['price'];
-			$vegie          = $restaurant['vegie'];
-			$opentime       = $restaurant['opentime'];
-			$closetime      = $restaurant['closetime'];
-			$id             = $restaurant['id'];
-			$new_restaurant = new Restaurant($name, $address, $phone, $price, $vegie, $opentime, $closetime, $id);
-			array_push($likes, $new_restaurant);
+		function __construct($new_username, $new_password, $new_vegie, $new_admin, $new_id = null)
+		{
+			$this->id       = $new_id;
+			$this->password = $new_password;
+			$this->username = $new_username;
+			$this->vegie    = $new_vegie;
+			$this->admin    = $new_admin;
 		}
-		return $likes;
-	}
-
-	function getDisLikes()
-	{
-		$user_likes = $GLOBALS['DB']->query("SELECT restaurants.* FROM users
-			JOIN likes ON  (users.id = likes.user_id)
-			JOIN restaurants ON (likes.restaurant_id = restaurants.id)
-			WHERE likes.answer = 0 AND likes.user_id = {$this->getId()};");
-		$likes      = array();
-		foreach ($user_likes as $restaurant) {
-			$name           = $restaurant['name'];
-			$address        = $restaurant['address'];
-			$phone          = $restaurant['phone'];
-			$price          = $restaurant['price'];
-			$vegie          = $restaurant['vegie'];
-			$opentime       = $restaurant['opentime'];
-			$closetime      = $restaurant['closetime'];
-			$id             = $restaurant['id'];
-			$new_restaurant = new Restaurant($name, $address, $phone, $price, $vegie, $opentime, $closetime, $id);
-			array_push($likes, $new_restaurant);
+		//setters
+		function setUsername($new_username)
+		{
+			$this->username = $new_username;
 		}
-		return $likes;
-	}
 
-	//static functions
-	static function getAll()
-	{
-		$returned_users = $GLOBALS['DB']->query("SELECT * FROM users ;");
-		$users = array();
-		foreach ($returned_users as $user) {
-			$username = $user['username'];
-			$password = $user['password'];
-			$admin    = $user['admin'];
-			$vegie    = $user['vegie'];
-			$id       = $user['id'];
-			$new_user = new User($username, $password, $vegie, $admin, $id);
-			array_push($users, $new_user);
+		function setPassword($new_password)
+		{
+			$this->password = $new_password;
 		}
-		return $users;
-	}
 
-	static function deleteAll()
-	{
-		$GLOBALS['DB']->exec("DELETE FROM users *;");
-	}
+		function setVegie($new_vegie)
+		{
+			$this->vegie = $new_vegie;
+		}
 
-	static function find($search_id)
-	{
-		$returned_user = null;
-		$all_users     = User::getAll();
-		foreach ($all_users as $user) {
-			$user_id = $user->getId();
-			if ($user_id == $search_id) {
-				$returned_user = $user;
+		function setAdmin($new_admin)
+		{
+			$this->admin = $new_admin;
+		}
+
+		function setId($new_id)
+		{
+			$this->id = (int) $new_id;
+		}
+		//getters
+		function getUsername()
+		{
+			return $this->username;
+		}
+
+		function getPassword()
+		{
+			return $this->password;
+		}
+
+		function getVegie()
+		{
+			return $this->vegie;
+		}
+
+		function getAdmin()
+		{
+			return $this->admin;
+		}
+
+		function getId()
+		{
+			return $this->id;
+		}
+
+		function save()
+		{
+			$statemnt = $GLOBALS['DB']->query("INSERT INTO users (username, password,vegie,admin) VALUES
+				('{$this->getUsername()}', '{$this->getPassword()}',{$this->getVegie()},{$this->getAdmin()}) RETURNING id;");
+			$result   = $statemnt->fetch(PDO::FETCH_ASSOC);
+			$this->setId($result['id']);
+		}
+
+		function updateUsername($user_name)
+		{
+			$GLOBALS['DB']->exec("UPDATE users SET username={$user_name} WHERE id={$this->getId()};");
+			$this->setUsername($user_name);
+		}
+
+		function updatePassword($new_password)
+		{
+			$GLOBALS['DB']->exec("UPDATE users SET password={$new_password} WHERE id={$this->getId()};");
+			$this->setPassword($new_password);
+		}
+
+		function updateAnswer($answer, $rest_id)
+		{
+			$GLOBALS['DB']->exec("UPDATE likes SET answer = {$answer} Where user_id={$this->getId() } AND restaurant_id={$rest_id};");
+		}
+
+		function addAnswer($user_id, $res_id, $answer)
+		{
+			$GLOBALS['DB']->exec(" INSERT INTO likes (answer, user_id, restaurant_id)
+				VALUES ($answer,{$this->getId()},$res_id)");
+		}
+
+		function delete()
+		{
+			$GLOBALS['DB']->exec("DELETE FROM users * WHERE id={$this->getId()};");
+		}
+
+		function getLikes()
+		{
+			$user_likes = $GLOBALS['DB']->query("SELECT restaurants.* FROM users
+				JOIN likes ON (users.id = likes.user_id)
+				JOIN restaurants ON (likes.restaurant_id = restaurants.id)
+				WHERE likes.answer = 2 AND likes.user_id = {$this->getId()};");
+			$likes = array();
+			foreach ($user_likes as $restaurant) {
+				$name           = $restaurant['name'];
+				$address        = $restaurant['address'];
+				$phone          = $restaurant['phone'];
+				$price          = $restaurant['price'];
+				$vegie          = $restaurant['vegie'];
+				$opentime       = $restaurant['opentime'];
+				$closetime      = $restaurant['closetime'];
+				$id             = $restaurant['id'];
+				$new_restaurant = new Restaurant($name, $address, $phone, $price, $vegie, $opentime, $closetime, $id);
+				array_push($likes, $new_restaurant);
 			}
+			return $likes;
 		}
-		return $returned_user;
-	}
 
-	static function CheckUsers($username){
-		//if a user already exists, CheckUsers will return 1. If not, the user can be created and CheckUsers will return 0.
-	    $result = 0;
-	    $all_users= User::getAll();
-	    foreach($all_users as $user){
-			$user_username= $user->getUsername();
-			if($user_username == $username){
-				$result = 1;
+		function getDisLikes()
+		{
+			$user_likes = $GLOBALS['DB']->query("SELECT restaurants.* FROM users
+				JOIN likes ON  (users.id = likes.user_id)
+				JOIN restaurants ON (likes.restaurant_id = restaurants.id)
+				WHERE likes.answer = 0 AND likes.user_id = {$this->getId()};");
+			$likes      = array();
+			foreach ($user_likes as $restaurant) {
+				$name           = $restaurant['name'];
+				$address        = $restaurant['address'];
+				$phone          = $restaurant['phone'];
+				$price          = $restaurant['price'];
+				$vegie          = $restaurant['vegie'];
+				$opentime       = $restaurant['opentime'];
+				$closetime      = $restaurant['closetime'];
+				$id             = $restaurant['id'];
+				$new_restaurant = new Restaurant($name, $address, $phone, $price, $vegie, $opentime, $closetime, $id);
+				array_push($likes, $new_restaurant);
 			}
+			return $likes;
 		}
-		return $result;
-	}
 
-	static function CheckPassword($username, $password)
-	{
-		//0 means there is a match, which we want
-		$result = 1;
-		$all_users= User::getAll();
-		 foreach($all_users as $user){
-			$user_username= $user->getUsername();
-			$user_password= $user->getPassword();
-			if($user_username == $username && $user_password == $password){
-				$result = 0;
-
+		//static functions
+		static function getAll()
+		{
+			$returned_users = $GLOBALS['DB']->query("SELECT * FROM users ;");
+			$users = array();
+			foreach ($returned_users as $user) {
+				$username = $user['username'];
+				$password = $user['password'];
+				$admin    = $user['admin'];
+				$vegie    = $user['vegie'];
+				$id       = $user['id'];
+				$new_user = new User($username, $password, $vegie, $admin, $id);
+				array_push($users, $new_user);
 			}
+			return $users;
 		}
-		return $result;
+
+		static function deleteAll()
+		{
+			$GLOBALS['DB']->exec("DELETE FROM users *;");
+		}
+
+		static function find($search_id)
+		{
+			$returned_user = null;
+			$all_users     = User::getAll();
+			foreach ($all_users as $user) {
+				$user_id = $user->getId();
+				if ($user_id == $search_id) {
+					$returned_user = $user;
+				}
+			}
+			return $returned_user;
+		}
+
+		static function CheckUsers($username){
+			//if a user already exists, CheckUsers will return 1. If not, the user can be created and CheckUsers will return 0.
+		    $result = 0;
+		    $all_users= User::getAll();
+		    foreach($all_users as $user){
+				$user_username= $user->getUsername();
+				if($user_username == $username){
+					$result = 1;
+				}
+			}
+			return $result;
+		}
+
+		static function CheckPassword($username, $password)
+		{
+			//0 means there is a match, which we want
+			$result = 1;
+			$all_users= User::getAll();
+			 foreach($all_users as $user){
+				$user_username= $user->getUsername();
+				$user_password= $user->getPassword();
+				if($user_username == $username && $user_password == $password){
+					$result = 0;
+
+				}
+			}
+			return $result;
 
     }
 }
